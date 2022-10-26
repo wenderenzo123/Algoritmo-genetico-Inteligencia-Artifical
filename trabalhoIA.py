4b248bee74d989188e4ac619e577be6b38f09bde
Wender <wenderenzo2014@outlook.com>
2022-10-26 09:11:03 -0300

    alteraçoes

--- trabalhoIA.py
+++ trabalhoIA.py
@@ -1,14 +1,14 @@
 from math import sqrt
 import random as rd
 Population = 20
-generations = 5
+generations = 6
 listaX = []
 listaY = []
 listaIndividuosBin = []
 
 
 def convertIntToBinaryString(intValue, nBits):
-    # Converte um inteiro para uma string binária sem o prefixo '0b'
+    # Converte um inteiro para uma string binária sem o prefixo '0b' e com nBits
     binaryString = bin(intValue)[2:]
     if len(binaryString) < nBits:
         binaryString = '0' * (nBits - len(binaryString)) + binaryString
@@ -42,29 +42,29 @@ def inverterUltimoBit(binaryString):
 
 def mutation(listIndividuos):
     # Mutação de 10%
-    for i in range(int(len(listIndividuos) * 0.1)):
-        listIndividuos[i][0] = inverterUltimoBit(listIndividuos[i][0])
-        listIndividuos[i][1] = inverterUltimoBit(listIndividuos[i][1])
+    for i in range(int(len(listIndividuos))):
+        number = rd.randint(0, 100)  # Gera um número aleatório de 0 a 100
+        if number >= 90:
+            listIndividuos[i][0] = inverterUltimoBit(listIndividuos[i][0])
+            listIndividuos[i][1] = inverterUltimoBit(listIndividuos[i][1])
     return listIndividuos
 
 
 def crossover(listIndividuos):
     # Crossover de 90%
-    for i in range(int(len(listIndividuos) * 0.9)):
-        # Escolhe dois pais aleatoriamente
-        pai1 = rd.randint(0, len(listIndividuos) - 1)
-        pai2 = rd.randint(0, len(listIndividuos) - 1)
-        # Escolhe um ponto de corte aleatoriamente
-        corte = rd.randint(0, 2)
-        # Faz o crossover
-        filho1 = listIndividuos[pai1][0][:corte] + \
-            listIndividuos[pai2][0][corte:]
-        filho2 = listIndividuos[pai1][1][:corte] + \
-            listIndividuos[pai2][1][corte:]
-        # Adiciona os filhos na lista de individuos
-        listIndividuos.append([filho1, filho2])
+    for i in range(int(len(listIndividuos))):
+        number = rd.randint(0, 100)  # Gera um número aleatório de 0 a 100
+        if number < 90:
+            # Gera um ponto de corte aleatório de 1 a 7
+            point = rd.randint(0, 2)
+            # Gera um ponto de corte aleatório de 1 a 7
+            point2 = rd.randint(0, 2)
+            # Faz o crossover entre os dois pontos
+            listIndividuos[i][0] = listIndividuos[i][0][:point] + \
+                listIndividuos[i][0][point2:]
+            listIndividuos[i][1] = listIndividuos[i][1][:point] + \
+                listIndividuos[i][1][point2:]
     return listIndividuos
-
 
 for i in range(0, Population):
     listaX.append(rd.randint(0, 7))
@@ -80,7 +80,6 @@ while True:
     print('Fitness: ', listaIndividuosInt[0][-1])
     for i in listaIndividuosInt:
         print("Individuo: ", i)
-
     if listaIndividuosInt[0][-1] == 0:
         break
     listaIndividuosBin = []
@@ -97,6 +96,5 @@ while True:
              convertBinaryStringToInt(listaIndividuosBin[i][1])])
     listaIndividuosInt = fitnessFunction(listaIndividuosInt)
     generations -= 1
-
     if generations == 0:
         break
